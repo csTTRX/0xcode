8bb4ea853ef5a71839b2bd31d9189f20f4b40b8b
csTTRX <cs.ttrx@gmail.com>
2022-02-25 15:48:27 +0100

    retouch

--- scPortfolio/settings.py
+++ scPortfolio/settings.py
@@ -88,22 +88,22 @@ WSGI_APPLICATION = 'scPortfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'zeroxcode',
-#        'USER': 'ircode',
-#        'PASSWORD': '228kemite223',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zeroxcode',
+        'USER': 'ircode',
+        'PASSWORD': '228kemite223',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
