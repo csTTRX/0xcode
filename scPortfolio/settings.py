ab8896f579f92abb49125f9c8219afa21cf4336b
csTTRX <cs.ttrx@gmail.com>
2022-03-04 12:23:45 +0100

    retouch

--- scPortfolio/settings.py
+++ scPortfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z^2f(0!#qjz%6_+h%$wqjyr39wc7h!l0a3jk_^*^jy_15%#17e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
